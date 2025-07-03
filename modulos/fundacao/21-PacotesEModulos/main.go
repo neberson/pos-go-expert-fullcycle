a2c40c040586e95720759f75cb341313b48ec9ee
Neberson Andrade <neberson_mtv@hotmail.com>
2025-07-03 13:16:18 -0300

    - Implementa aula sobre modificadores de acesso em GO.

--- modulos/fundacao/21-PacotesEModulos/main.go
+++ modulos/fundacao/21-PacotesEModulos/main.go
@@ -8,5 +8,10 @@ import (
 
 func main() {
 	soma := matematica.Soma(10, 20)
+	carro := matematica.Carro{Marca: "Fiat"}
+
+	fmt.Println(carro.Andar())
+	fmt.Println(carro)
 	fmt.Println("Resultado: ", soma)
+	fmt.Println(matematica.A)
 }
