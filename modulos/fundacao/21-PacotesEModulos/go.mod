db34182ac15528132976f05109484b2d79116929
Neberson Andrade <neberson_mtv@hotmail.com>
2025-07-03 13:02:33 -0300

    - Implementa as aulas referente a pacotes e modules no GO

---
+++ modulos/fundacao/21-PacotesEModulos/go.mod
@@ -0,0 +1,3 @@
+module github.com/neberson/pos-go-expert-fullcycle/modulos/fundacao/21-PacotesEModulos
+
+go 1.24.4
---
+++ modulos/fundacao/21-PacotesEModulos/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/neberson/pos-go-expert-fullcycle/modulos/fundacao/21-PacotesEModulos/matematica"
+)
+
+func main() {
+	soma := matematica.Soma(10, 20)
+	fmt.Println("Resultado: ", soma)
+}
