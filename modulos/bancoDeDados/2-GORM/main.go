01c9aac40379e84c3b7f63e3ef46e0a65554025d
Neberson Andrade <neberson_mtv@hotmail.com>
2025-07-17 17:26:59 -0300

    - Implementa aula sobre alteração e remoção de registro.

--- modulos/bancoDeDados/2-GORM/main.go
+++ modulos/bancoDeDados/2-GORM/main.go
@@ -23,51 +23,61 @@ func main() {
 
 	db.AutoMigrate(&Product{})
 
-	db.Create(&Product{
-		Name:  "Notebook",
-		Price: 1000.00,
-	})
+	// db.Create(&Product{
+	// 	Name:  "Notebook",
+	// 	Price: 1000.00,
+	// })
 
-	products := []Product{
-		{Name: "Notebook", Price: 1000.00},
-		{Name: "Mouse", Price: 50.00},
-		{Name: "Keyboard", Price: 100.00},
-	}
+	// products := []Product{
+	// 	{Name: "Notebook", Price: 1000.00},
+	// 	{Name: "Mouse", Price: 50.00},
+	// 	{Name: "Keyboard", Price: 100.00},
+	// }
 
-	db.Create(&products)
+	// db.Create(&products)
 
-	//select one
-	var product Product
-	db.First(&product, 5)
-	fmt.Println(product)
+	// //select one
+	// var product Product
+	// db.First(&product, 5)
+	// fmt.Println(product)
 
-	db.First(&product, "name = ?", "Mouse")
-	fmt.Println(product)
+	// db.First(&product, "name = ?", "Mouse")
+	// fmt.Println(product)
 
-	//select all
-	var products2 []Product
-	db.Find(&products2)
-	for _, product := range products2 {
-		fmt.Println(product)
-	}
+	// //select all
+	// var products2 []Product
+	// db.Find(&products2)
+	// for _, product := range products2 {
+	// 	fmt.Println(product)
+	// }
 
-	var products3 []Product
-	db.Limit(2).Offset(3).Find(&products3)
-	for _, product := range products3 {
-		fmt.Println(product)
-	}
+	// var products3 []Product
+	// db.Limit(2).Offset(3).Find(&products3)
+	// for _, product := range products3 {
+	// 	fmt.Println(product)
+	// }
 
-	//where
-	var products4 []Product
-	db.Where("price > ?", 50).Find(&products4)
-	for _, product := range products4 {
-		fmt.Println(product)
-	}
+	// //where
+	// var products4 []Product
+	// db.Where("price > ?", 50).Find(&products4)
+	// for _, product := range products4 {
+	// 	fmt.Println(product)
+	// }
 
-	//where
-	var products5 []Product
-	db.Where("name LIKE ?", "%book%").Find(&products5)
-	for _, product := range products5 {
-		fmt.Println(product)
-	}
+	// //where
+	// var products5 []Product
+	// db.Where("name LIKE ?", "%book%").Find(&products5)
+	// for _, product := range products5 {
+	// 	fmt.Println(product)
+	// }
+
+	var p Product
+	db.First(&p, 1)
+	p.Name = "New Mouse"
+	db.Save(&p)
+
+	var p2 Product
+	db.First(&p2, 1)
+	fmt.Println(p2.Name)
+	db.Delete(&p2)
 }
