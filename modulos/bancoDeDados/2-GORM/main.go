3131a9b0a3c31dcb9feeb30925575bb6999b958f
Neberson Andrade <neberson_mtv@hotmail.com>
2025-07-21 12:32:49 -0300

    - Implementa aula sobre Has Many

--- modulos/bancoDeDados/2-GORM/main.go
+++ modulos/bancoDeDados/2-GORM/main.go
@@ -8,8 +8,9 @@ import (
 )
 
 type Category struct {
-	ID   int `gorm:"primaryKey"`
-	Name string
+	ID       int `gorm:"primaryKey"`
+	Name     string
+	Products []Product
 }
 
 type Product struct {
@@ -36,7 +37,7 @@ func main() {
 		panic(err)
 	}
 
-	db.AutoMigrate(&Category{}, &Product{}, SerialNumber{})
+	db.AutoMigrate(&Category{}, &Product{}, &SerialNumber{})
 
 	//create category
 	category := Category{Name: "Eletronicos"}
@@ -53,12 +54,24 @@ func main() {
 		Number:    "123456",
 		ProductID: product.ID,
 	})
-	var products []Product
 
-	db.Preload("Category").Preload("SerialNumber").Find(&products)
-	for _, product := range products {
-		fmt.Println(product.Name, product.Category.Name, product.SerialNumber.Number)
+	var categories []Category
+	err = db.Model(&Category{}).Preload("Products").Find(&categories).Error
+	if err != nil {
+		panic(err)
 	}
+	for _, category := range categories {
+		fmt.Println(category.Name, ":")
+		for _, product := range category.Products {
+			println("- ", product.Name)
+		}
+	}
+	// var products []Product
+
+	// db.Preload("Category").Preload("SerialNumber").Find(&products)
+	// for _, product := range products {
+	// 	fmt.Println(product.Name, product.Category.Name, product.SerialNumber.Number)
+	// }
 
 	// db.Create(&Product{
 	// 	Name:  "Notebook",
