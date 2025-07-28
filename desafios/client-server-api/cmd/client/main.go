caa726f1c03883af119237e988c04977b4785ab2
Neberson Andrade <neberson_mtv@hotmail.com>
2025-07-28 15:02:41 -0300

    - Implementa os requisitos de buscar cotação no client com timeout de 300ms.

--- desafios/client-server-api/cmd/client/main.go
+++ desafios/client-server-api/cmd/client/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"client-server-api/internal/services"
+	"fmt"
+	"log"
+)
 
+func main() {
+	cotacao, err := services.BuscaCotacaoClient()
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(cotacao)
 }
--- desafios/client-server-api/internal/services/cotacao.go
+++ desafios/client-server-api/internal/services/cotacao.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"client-server-api/pkg/entity"
+	"client-server-api/pkg/entity/dto"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -11,7 +12,8 @@ import (
 )
 
 const (
-	urlCotacao = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+	urlCotacao    = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+	urlBaseClient = "http://localhost:8080/cotacao"
 )
 
 func BuscaCotacao() (*entity.Cotacao, error) {
@@ -41,6 +43,36 @@ func BuscaCotacao() (*entity.Cotacao, error) {
 	}
 
 	var cotacao entity.Cotacao
+	if err = json.Unmarshal(body, &cotacao); err != nil {
+		return nil, fmt.Errorf("erro na conversão do corpo da requisição: %v", err.Error())
+	}
+
+	return &cotacao, nil
+}
+
+func BuscaCotacaoClient() (*dto.CotacaoDto, error) {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 300*time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", urlCotacao, nil)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao realizar a requisição da cotação: %v", err.Error())
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao realizar a requisição da cotação: %v", err.Error())
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao ler o corpo da requisição: %v", err.Error())
+	}
+
+	var cotacao dto.CotacaoDto
 	if err = json.Unmarshal(body, &cotacao); err != nil {
 		return nil, fmt.Errorf("erro na conversão do corpo da requisição: %v", err.Error())
 	}
