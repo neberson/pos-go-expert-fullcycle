9c52b82de28d98345eee6d73bb17b9fb9671578e
Neberson Andrade <neberson_mtv@hotmail.com>
2025-08-04 13:50:25 -0300

    - Implementa aula sobre acesso de pacotes criados.

---
+++ modulos/Packaging/2-AcessandoPacotesCriados/cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/neberson/pos-go-expert-fullcycle/tree/main/modulos/Packaging/2-AcessandoPacotesCriados/math"
+
+func main() {
+	m := math.Math{A: 1, B: 2}
+	println(m.Add())
+}
---
+++ modulos/Packaging/2-AcessandoPacotesCriados/go.mod
@@ -0,0 +1,3 @@
+module github.com/neberson/pos-go-expert-fullcycle/tree/main/modulos/Packaging/2-AcessandoPacotesCriados
+
+go 1.24.4
