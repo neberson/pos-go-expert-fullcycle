332a03a6f341d11c81e3788ddaacebe0ad87eab2
Neberson Andrade <neberson_mtv@hotmail.com>
2025-08-06 12:07:26 -0300

    - Implementa aula trabalhando com mocks

---
+++ modulos/Testing/7-TrabalhandoComMocks/tax_test.go
@@ -0,0 +1,35 @@
+package tax
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCalculateTax(t *testing.T) {
+	tax, err := CalculateTax(1000.0)
+	assert.Nil(t, err)
+	assert.Equal(t, 10.0, tax)
+
+	tax, err = CalculateTax(0)
+	assert.Error(t, err, "amount must be greater than 0")
+	assert.Equal(t, 0.0, tax)
+
+	assert.Contains(t, err.Error(), "greater than 0")
+}
+
+func TestCalculateTaxAndSave(t *testing.T) {
+	repository := &TaxRepositoryMock{}
+	repository.On("SaveTax", 10.0).Return(nil)
+	repository.On("SaveTax", 0.0).Return(errors.New("error save tax"))
+
+	err := CalculateTaxAndSave(1000.0, repository)
+	assert.Nil(t, err)
+
+	err = CalculateTaxAndSave(0.0, repository)
+	assert.Error(t, err, "error save tax")
+
+	repository.AssertExpectations(t)
+	repository.AssertNumberOfCalls(t, "SaveTax", 2)
+}
