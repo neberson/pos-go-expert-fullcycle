509cfa42962d82e8ee5ba3575599f3ce93aea1b5
Neberson Andrade <neberson_mtv@hotmail.com>
2025-08-07 12:51:30 -0300

    - Finaliza as configurações da API - Implementa struct user

--- modulos/API/cmd/server/main.go
+++ modulos/API/cmd/server/main.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import "github.com/neberson/pos-go-expert-fullcycle/tree/main/modulos/API/configs"
 
+func main() {
+	config, _ := configs.LoadConfig(".")
+	println(config.DBDriver)
 }
