71c8803a6d00ef84e28c3ad95cd461e588238544
Neberson Andrade <neberson_mtv@hotmail.com>
2025-08-12 11:27:15 -0300

    - Implementa entidade de produto e testes para produto

---
+++ modulos/API/internal/entity/product_test.go
@@ -0,0 +1,45 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewProduct(t *testing.T) {
+	product, err := NewProduct("Product 1", 10)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, product)
+	assert.NotEmpty(t, product.ID)
+	assert.Equal(t, "Product 1", product.Name)
+	assert.Equal(t, 10, product.Price)
+}
+
+func TestProductWhenNameIsRequired(t *testing.T) {
+	_, err := NewProduct("", 10)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, ErrNameIsRequired, err)
+}
+
+func TestProductWhenPriceIsRequired(t *testing.T) {
+	_, err := NewProduct("Product 1", 0)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, ErrPriceIsRequired, err)
+}
+
+func TestProductWhenPriceIsInvalid(t *testing.T) {
+	_, err := NewProduct("Product 1", -10)
+
+	assert.NotNil(t, err)
+	assert.Equal(t, ErrInvalidPrice, err)
+}
+
+func TestProductValidate(t *testing.T) {
+	product, err := NewProduct("Product 1", 10)
+	assert.Nil(t, err)
+	assert.NotNil(t, product)
+	assert.Nil(t, product.Validate())
+}
