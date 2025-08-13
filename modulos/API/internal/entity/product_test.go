86caf8a7f1385daffee203766cb937329025e62c
Neberson Andrade <neberson_mtv@hotmail.com>
2025-08-13 11:27:45 -0300

    - Implementa testes para product.

--- modulos/API/internal/entity/product_test.go
+++ modulos/API/internal/entity/product_test.go
@@ -7,38 +7,38 @@ import (
 )
 
 func TestNewProduct(t *testing.T) {
-	product, err := NewProduct("Product 1", 10)
+	product, err := NewProduct("Product 1", 10.0)
 
 	assert.Nil(t, err)
 	assert.NotNil(t, product)
 	assert.NotEmpty(t, product.ID)
 	assert.Equal(t, "Product 1", product.Name)
-	assert.Equal(t, 10, product.Price)
+	assert.Equal(t, 10.0, product.Price)
 }
 
 func TestProductWhenNameIsRequired(t *testing.T) {
-	_, err := NewProduct("", 10)
+	_, err := NewProduct("", 10.0)
 
 	assert.NotNil(t, err)
 	assert.Equal(t, ErrNameIsRequired, err)
 }
 
 func TestProductWhenPriceIsRequired(t *testing.T) {
-	_, err := NewProduct("Product 1", 0)
+	_, err := NewProduct("Product 1", 0.0)
 
 	assert.NotNil(t, err)
 	assert.Equal(t, ErrPriceIsRequired, err)
 }
 
 func TestProductWhenPriceIsInvalid(t *testing.T) {
-	_, err := NewProduct("Product 1", -10)
+	_, err := NewProduct("Product 1", -10.0)
 
 	assert.NotNil(t, err)
 	assert.Equal(t, ErrInvalidPrice, err)
 }
 
 func TestProductValidate(t *testing.T) {
-	product, err := NewProduct("Product 1", 10)
+	product, err := NewProduct("Product 1", 10.0)
 	assert.Nil(t, err)
 	assert.NotNil(t, product)
 	assert.Nil(t, product.Validate())
