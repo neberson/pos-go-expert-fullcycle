a3b848b44798186ecb152c54321e4bfb78a20241
Neberson Andrade <neberson_mtv@hotmail.com>
2025-08-14 10:34:15 -0300

    - Implementa arquitetura adequada para os handlers.

--- modulos/API/cmd/server/main.go
+++ modulos/API/cmd/server/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/glebarez/sqlite"
 	"github.com/neberson/pos-go-expert-fullcycle/tree/main/modulos/API/configs"
-	"github.com/neberson/pos-go-expert-fullcycle/tree/main/modulos/API/internal/dto"
 	"github.com/neberson/pos-go-expert-fullcycle/tree/main/modulos/API/internal/entity"
 	"github.com/neberson/pos-go-expert-fullcycle/tree/main/modulos/API/internal/infra/database"
+	"github.com/neberson/pos-go-expert-fullcycle/tree/main/modulos/API/internal/infra/webserver/handlers"
 	"gorm.io/gorm"
 )
 
@@ -24,41 +23,8 @@ func main() {
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
 
 	productDB := database.NewProduct(db)
-	productHandler := NewProductHandler(productDB)
+	productHandler := handlers.NewProductHandler(productDB)
 	http.HandleFunc("/products", productHandler.CreateProduct)
 
 	http.ListenAndServe(":8000", nil)
-}
-
-type ProductHandler struct {
-	ProductDB database.ProductInterface
-}
-
-func NewProductHandler(db database.ProductInterface) *ProductHandler {
-	return &ProductHandler{
-		ProductDB: db,
-	}
-}
-
-func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
-	var product dto.CreateProductInput
-	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	p, err := entity.NewProduct(product.Name, product.Price)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	err = h.ProductDB.Create(p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(product)
 }
---
+++ modulos/API/internal/infra/webserver/handlers/product_handlers.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/neberson/pos-go-expert-fullcycle/tree/main/modulos/API/internal/dto"
+	"github.com/neberson/pos-go-expert-fullcycle/tree/main/modulos/API/internal/entity"
+	"github.com/neberson/pos-go-expert-fullcycle/tree/main/modulos/API/internal/infra/database"
+)
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(product)
+}
