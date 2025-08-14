faa63a305cf55cb4496f9801072e7ad049a44850
Neberson Andrade <neberson_mtv@hotmail.com>
2025-08-14 12:14:11 -0300

    - Implementa post para user.

--- modulos/API/cmd/server/main.go
+++ modulos/API/cmd/server/main.go
@@ -27,6 +27,9 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Post("/products", productHandler.CreateProduct)
@@ -34,6 +37,8 @@ func main() {
 	r.Put("/products/{id}", productHandler.UpdateProduct)
 	r.Delete("/products/{id}", productHandler.DeleteProduct)
 	r.Get("/products", productHandler.GetAllProducts)
+
+	r.Post("/users", userHandler.CreateUser)
 
 	http.ListenAndServe(":8000", r)
 }
