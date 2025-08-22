0dcb7a3270594f163b32618d565ec251efb4dee0
neberson <neberson_mtv@hotmail.com>
2025-08-21 22:59:50 -0300

    - Implementa aula sobre go mod vendor

---
+++ modulos/ModulosPrivados/vendor/github.com/neberson/fcutils/pkg/events/event_dispatcher.go
@@ -0,0 +1,71 @@
+package events
+
+import (
+	"errors"
+	"sync"
+)
+
+var errorHandlerAlreadyRegistered = errors.New("handler already registered")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+	if _, exists := ed.handlers[eventName]; exists {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return errorHandlerAlreadyRegistered
+			}
+		}
+	}
+
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+}
+
+func (ed *EventDispatcher) Has(eventName string, handler EventHandlerInterface) bool {
+	if handlers, exists := ed.handlers[eventName]; exists {
+		for _, h := range handlers {
+			if h == handler {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (ed *EventDispatcher) Dispatch(event EventInterface) error {
+	if handlers, exists := ed.handlers[event.GetName()]; exists {
+		wg := &sync.WaitGroup{}
+		for _, handler := range handlers {
+			wg.Add(1)
+			go handler.Handle(event, wg)
+		}
+		wg.Wait()
+	}
+
+	return nil
+}
+
+func (ed *EventDispatcher) Remove(eventName string, handler EventHandlerInterface) error {
+	if _, exists := ed.handlers[eventName]; exists {
+		for i, h := range ed.handlers[eventName] {
+			if h == handler {
+				ed.handlers[eventName] = append(ed.handlers[eventName][:i], ed.handlers[eventName][i+1:]...)
+				return nil
+			}
+		}
+	}
+	return nil
+}
+
+func (ed *EventDispatcher) Clear() {
+	ed.handlers = make(map[string][]EventHandlerInterface)
+}
