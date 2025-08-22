a6a1f8022fb7c5aa6d9d3bd81572f14967a633e1
neberson <neberson_mtv@hotmail.com>
2025-08-22 13:20:36 -0300

    - Impementa encadeamento da category

--- modulos/GraphQL/graph/schema.resolvers.go
+++ modulos/GraphQL/graph/schema.resolvers.go
@@ -6,6 +6,7 @@ package graph
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/neberson/pos-go-expert-fullcycle/modulos/GraphQL/graph/model"
 )
@@ -25,6 +26,19 @@ func (r *categoryResolver) Courses(ctx context.Context, obj *model.Category) ([]
 		})
 	}
 	return coursesModel, nil
+}
+
+// Category is the resolver for the category field.
+func (r *courseResolver) Category(ctx context.Context, obj *model.Course) (*model.Category, error) {
+	category, err := r.CategoryDB.FindByCourseID(obj.ID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find category for course %s: %w", obj.ID, err)
+	}
+	return &model.Category{
+		ID:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}, nil
 }
 
 // CreateCategory is the resolver for the createCategory field.
@@ -90,6 +104,9 @@ func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
 // Category returns CategoryResolver implementation.
 func (r *Resolver) Category() CategoryResolver { return &categoryResolver{r} }
 
+// Course returns CourseResolver implementation.
+func (r *Resolver) Course() CourseResolver { return &courseResolver{r} }
+
 // Mutation returns MutationResolver implementation.
 func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
 
@@ -97,5 +114,6 @@ func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
 func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 
 type categoryResolver struct{ *Resolver }
+type courseResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
