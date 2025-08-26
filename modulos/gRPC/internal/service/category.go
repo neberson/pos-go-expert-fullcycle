4896b7bb2dc59d41eb0b2af4cc813c113612ae30
neberson <neberson_mtv@hotmail.com>
2025-08-26 12:24:57 -0300

    - Implementa create de category

---
+++ modulos/gRPC/internal/service/category.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"context"
+
+	"github.com/neberson/pos-go-expert-fullcycle/modulos/gRPC/internal/database"
+	"github.com/neberson/pos-go-expert-fullcycle/modulos/gRPC/internal/pb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (c CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := c.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to create category: %v", err)
+	}
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CategoryResponse{Category: categoryResponse}, nil
+}
