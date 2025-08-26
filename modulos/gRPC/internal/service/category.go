a1308d1ea018830402fd8a16900c7977162c9acc
neberson <neberson_mtv@hotmail.com>
2025-08-26 19:01:58 -0300

    - Implementa o conceito de stream no gRPC

--- modulos/gRPC/internal/service/category.go
+++ modulos/gRPC/internal/service/category.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/neberson/pos-go-expert-fullcycle/modulos/gRPC/internal/database"
 	"github.com/neberson/pos-go-expert-fullcycle/modulos/gRPC/internal/pb"
@@ -63,4 +64,29 @@ func (c CategoryService) GetCategory(ctx context.Context, in *pb.CategoryGetRequ
 		Description: category.Description,
 	}
 	return categoryResponse, nil
+}
+
+func (c CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+	}
 }
