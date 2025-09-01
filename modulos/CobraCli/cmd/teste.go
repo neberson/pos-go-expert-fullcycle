03276c338a3e2bfb07d0b7b213428b7a5a076ff7
neberson <neberson_mtv@hotmail.com>
2025-09-01 13:24:25 -0300

    - Implementando primeiros comandos.

--- modulos/CobraCli/cmd/teste.go
+++ modulos/CobraCli/cmd/teste.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -10,9 +9,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// pongCmd represents the pong command
-var pongCmd = &cobra.Command{
-	Use:   "pong",
+// testeCmd represents the teste command
+var testeCmd = &cobra.Command{
+	Use:   "teste",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,20 +20,36 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("pong called")
+		commando, err := cmd.Flags().GetString("--comando")
+		if err != nil {
+			fmt.Println("Erro ao ler o comando:", err)
+			return
+		}
+
+		switch commando {
+		case "ping":
+			fmt.Println("ping")
+		case "pong":
+			fmt.Println("pong")
+		default:
+			fmt.Println("Comando desconhecido. Use 'ping' ou 'pong'.")
+		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(pongCmd)
+	rootCmd.AddCommand(testeCmd)
+
+	testeCmd.Flags().StringP("--comando", "c", "", "Escolha ping ou pong")
+	testeCmd.MarkFlagRequired("--comando")
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// pongCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// testeCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// pongCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// testeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
