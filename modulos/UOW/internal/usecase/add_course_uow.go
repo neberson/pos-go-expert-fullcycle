b22b7edfc83d3366d102e3378c4fd7a13d2ecb6c
neberson <neberson_mtv@hotmail.com>
2025-09-23 21:50:36 -0300

    - Implementa usecase para unit of work

---
+++ modulos/UOW/internal/usecase/add_course_uow.go
@@ -0,0 +1,69 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/neberson/pos-go-expert-fullcycle/modulos/UOW/internal/entity"
+	"github.com/neberson/pos-go-expert-fullcycle/modulos/UOW/internal/repository"
+	"github.com/neberson/pos-go-expert-fullcycle/modulos/UOW/pkg/uow"
+)
+
+type InputUseCaseUow struct {
+	CategoryName     string
+	CourseName       string
+	CourseCategoryID int
+}
+
+type AddCourseUseCaseUow struct {
+	Uow uow.UowInterface
+}
+
+func NewAddCourseUseCaseUow(uow uow.UowInterface) *AddCourseUseCaseUow {
+	return &AddCourseUseCaseUow{
+		Uow: uow,
+	}
+}
+
+func (a *AddCourseUseCaseUow) Execute(ctx context.Context, input InputUseCase) error {
+	return a.Uow.Do(ctx, func(uow uow.UowInterface) error {
+		// tudo que colocarmos aqui, será feito um begin e um commit ou rollback
+		// se der erro em qualquer parte, tudo será desfeito (rollback)
+		category := entity.Category{
+			Name: input.CategoryName,
+		}
+		err := a.getCategoryRepository(ctx).Insert(ctx, category)
+		if err != nil {
+			return err
+		}
+
+		course := entity.Course{
+			Name:       input.CourseName,
+			CategoryID: input.CourseCategoryID,
+		}
+
+		repoCourse := a.getCourseRepository(ctx)
+		err = repoCourse.Insert(ctx, course)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+}
+
+func (a *AddCourseUseCaseUow) getCategoryRepository(ctx context.Context) repository.CategoryRepositoryInterface {
+	repo, err := a.Uow.GetRepository(ctx, "CategoryRepository")
+	if err != nil {
+		panic(err)
+	}
+	return repo.(repository.CategoryRepositoryInterface)
+}
+
+func (a *AddCourseUseCaseUow) getCourseRepository(ctx context.Context) repository.CourseRepositoryInterface {
+	repo, err := a.Uow.GetRepository(ctx, "CourseRepository")
+	if err != nil {
+		panic(err)
+	}
+	return repo.(repository.CourseRepositoryInterface)
+}
