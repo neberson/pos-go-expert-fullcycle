45dddcc0ee03436aa89834106b121044d3e3bc95
neberson <neberson_mtv@hotmail.com>
2025-10-02 12:15:42 -0300

    - Implementa usecase para order

---
+++ modulos/CleanArchitecture/internal/usecase/create_order.go
@@ -0,0 +1,61 @@
+package usecase
+
+import (
+	"github.com/neberson/pos-go-expert-fullcycle/modulos/CleanArchitecture/internal/entity"
+	"github.com/neberson/pos-go-expert-fullcycle/modulos/CleanArchitecture/internal/event"
+)
+
+type OrderInputDTO struct {
+	ID    string  `json:"id"`
+	Price float64 `json:"price"`
+	Tax   float64 `json:"tax"`
+}
+
+type OrderOutputDTO struct {
+	ID         string  `json:"id"`
+	Price      float64 `json:"price"`
+	Tax        float64 `json:"tax"`
+	FinalPrice float64 `json:"final_price"`
+}
+
+type CreateOrderUseCase struct {
+	OrderRepository entity.OrderRepositoryInterface
+	OrderCreated    event.EventInterface
+	EventDispatcher event.EventDispatcherInterface
+}
+
+func NewCreateOrderUseCase(
+	orderRepository entity.OrderRepositoryInterface,
+	orderCreated event.EventInterface,
+	eventDispatcher event.EventDispatcherInterface,
+) *CreateOrderUseCase {
+	return &CreateOrderUseCase{
+		OrderRepository: orderRepository,
+		OrderCreated:    orderCreated,
+		EventDispatcher: eventDispatcher,
+	}
+}
+
+func (c *CreateOrderUseCase) Execute(input OrderInputDTO) (OrderOutputDTO, error) {
+	order := entity.Order{
+		ID:    input.ID,
+		Price: input.Price,
+		Tax:   input.Tax,
+	}
+	order.CalculateFinalPrice()
+	if err := c.OrderRepository.Save(&order); err != nil {
+		return OrderOutputDTO{}, err
+	}
+
+	dto := OrderOutputDTO{
+		ID:         order.ID,
+		Price:      order.Price,
+		Tax:        order.Tax,
+		FinalPrice: order.FinalPrice,
+	}
+
+	c.OrderCreated.SetPayload(dto)
+	c.EventDispatcher.Dispatch(c.OrderCreated)
+
+	return dto, nil
+}
