18c5fad493d76995e103bffcaf7f99163cccb263
neberson <neberson_mtv@hotmail.com>
2025-10-06 22:20:05 -0300

    - Implementação injeção de dependencia com Google Wire.

--- modulos/CleanArchitecture/cmd/ordersystem/wire.go
+++ modulos/CleanArchitecture/cmd/ordersystem/wire.go
@@ -3,9 +3,26 @@
 
 package main
 
-import "github.com/google/wire"
+import (
+	"github.com/google/wire"
+	"ithub.com/neberson/pos-go-expert-fullcycle/modulos/CleanArchitecture/internal/event"
+	"ithub.com/neberson/pos-go-expert-fullcycle/modulos/CleanArchitecture/internal/infra/database"
+	"ithub.com/neberson/pos-go-expert-fullcycle/modulos/CleanArchitecture/pkg/events"
+)
 
 var SetOrderRepository = wire.NewSet(
 	database.NewOrderRepository,
 	wire.Bind(new(order.RepositoryInterface), new(*database.OrderRepository)),
+)
+
+var setEventDispatcherDependency = wire.NewSet(
+	events.NewEventDispatcher,
+	event.NewOrderCreated,
+	wire.Bind(new(events.EventInterface), new(*event.OrderCreated)),
+	wire.Bind(new(events.EventDispatcherInterface), new(*events.EventDispatcher)),
+)
+
+var setOrderCreatedEvent = wire.NewSet(
+	event.NewOrderCreated,
+	wire.Bind(new(events.EventInterface), new(*event.OrderCreated)),
 )
